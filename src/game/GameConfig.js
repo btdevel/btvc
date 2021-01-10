7fcae1ae7039cbbe06be2ebf72a85db990875f75
Elmar Zander <elmar@zandere.de>
2021-01-10 11:57:33 +0100

    feat: Make yaml config recursive

--- src/game/GameConfig.js
+++ src/game/GameConfig.js
@@ -12,15 +12,36 @@ function mergeObject(obj1, obj2) {
   for (const prop in obj2) {
     const value = obj2[prop]
     if (Array.isArray(value)) {
-      obj1[prop] = mergeArray(obj1[prop], value) 
+      obj1[prop] = mergeArray(obj1[prop], value)
     }
     else if (typeof value === 'object') {
-      obj1[prop] = mergeObject(obj1[prop], value) 
+      obj1[prop] = mergeObject(obj1[prop], value)
     } else {
       obj1[prop] = value
     }
   }
   return obj1
+}
+
+function mergeRecursive(name, yaml, cache) {
+  console.log('Recursively merging: ', name)
+  if (cache[name]) {
+    return cache[name]
+  }
+
+  const object = yaml.configs[name]
+  cache[name] = object
+
+  if (!object) {
+    console.warn('Could not find config: ', name)
+    return {}
+  }
+
+  for (const baseName of object.configs || []) {
+    const newObject = mergeRecursive(baseName, yaml, cache)
+    mergeObject(object, newObject)
+  }
+  return object
 }
 
 async function loadYAML(file) {
@@ -30,14 +51,13 @@ async function loadYAML(file) {
   return YAML.safeLoad(body)
 }
 
-export async function init (configfile, finished) {
-  const fullconf = await loadYAML(configfile)
-  console.log('Full config: ', fullconf);
-  let config = fullconf.init
-  for (const name of fullconf.debug) {
-    mergeObject(config, fullconf.debugConfigs[name])
-  }
-  config.keyMap = fullconf.keyMap
+export async function init(configFile, finished) {
+  const yaml = await loadYAML(configFile)
+  console.log('Full config: ', yaml)
+
+  const configName = yaml.config
+  const config = mergeRecursive(configName, yaml, {})
+  config.keyMap = yaml.keyMap
 
   console.log('Init config: \n', YAML.safeDump(config))
   gameState.init(config)
