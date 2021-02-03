7675cb48cdbe4e404f42f96e2f3f9d9db64ab4cf
Elmar Zander <elmar@zandere.de>
2021-02-03 23:32:17 +0100

    Add gesture handling to GameControls

--- src/pages/GameControls.js
+++ src/pages/GameControls.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { handleKeyDown } from '../game/KeyMap'
 import { handleMouseDown, handleMouseMove, handleMouseUp } from '../game/MouseHandling'
+import Hammer from 'hammerjs'
+import { gameState } from '../game/GameLogic'
+
 
 export default function GameControls() {
   useEffect(() => {
@@ -22,6 +25,20 @@ export default function GameControls() {
     console.log("Adding event listener: ", element)
     if (element) {
       const useCapture = false
+
+      const el = document.getElementById('gamescreen')
+      const gestures = new Hammer(el);
+      gestures.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
+      gestures.on("swipe", function (ev) {
+        console.log("swipe event: ", ev);
+      });
+      const nm = (ev) => (ev.pointerType !== "mouse")
+      gestures.on('swipeleft', (ev) => nm(ev) && gameState.turn(1))
+      gestures.on('swiperight', (ev) => nm(ev) && gameState.turn(-1))
+      gestures.on('swipeup', (ev) => nm(ev) && gameState.move(true))
+      gestures.on('swipedown', (ev) => nm(ev) && gameState.move(false))
+
+
       element.addEventListener('mousedown', handleMouseDown, useCapture)
       element.addEventListener('mousemove', handleMouseMove, useCapture)
       element.addEventListener('mouseup', handleMouseUp, useCapture)
@@ -33,6 +50,8 @@ export default function GameControls() {
         element.removeEventListener('mousemove', handleMouseMove)
         element.removeEventListener('mouseup', handleMouseUp)
         element.removeEventListener('mouseleave', handleMouseUp)
+        gestures.stop()
+        gestures.destroy()
       }
     }
   }, [element])
