7c56491d23b73d302e2d7fb1e971d3d2567cf94f
Elmar Zander <elmar@zandere.de>
2022-12-03 17:17:55 +0100

    Add merging for dungeon levels

--- src/game/DungeonMap.js
+++ src/game/DungeonMap.js
@@ -1,5 +1,6 @@
 import {setGameText} from './GameLogic'
 import Map, {create2dArray} from './Map'
+import {mergeObject} from "../util/merging";
 
 export default class DungeonMap extends Map {
   width
@@ -101,6 +102,11 @@ export async function loadMap(level) {
     const levelAmendImport = import(`../assets/levels/level_${levelNumPadded}_amend.json`)
     const levelExtra = (await levelAmendImport).default
     Object.assign(map, levelExtra)
+
+    if( map._merge) {
+      mergeObject(map, map._merge)
+      delete map._merge
+    }
   } catch {
     console.warn(`Could not load level ${level}`)
   }
---
+++ src/util/merging.js
@@ -0,0 +1,20 @@
+function mergeArray(obj1, obj2) {
+  if (!obj1) return obj2
+  return [...obj1, ...obj2]
+}
+
+export function mergeObject(obj1, obj2) {
+  if (!obj1) return obj2
+
+  for (const prop in obj2) {
+    const value = obj2[prop]
+    if (Array.isArray(value)) {
+      obj1[prop] = mergeArray(obj1[prop], value)
+    } else if (typeof value === 'object') {
+      obj1[prop] = mergeObject(obj1[prop], value)
+    } else {
+      obj1[prop] = value
+    }
+  }
+  return obj1
+}
