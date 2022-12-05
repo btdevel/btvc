abd0c18e78f67576bae8d706ba0a885927ab5ab6
Elmar Zander <elmar@zandere.de>
2022-12-05 08:57:27 +0100

    Improve Sphere video screen

--- src/3d/VideoScreen.js
+++ src/3d/VideoScreen.js
@@ -9,26 +9,18 @@ const w = 0.0, b = 0.0, t = 0.0
 const screenGeom = makeShapeGeometry([[w, b], [w, 1 - t], [1 - w, 1 - t], [1 - w, b]])
 screenGeom.scale(0.9, 0.6, 1).translate(0, 0.046, 0)
 
-const sphereGeom = new THREE.SphereGeometry(0.2, 20, 20)
+const sphereGeom = new THREE.SphereGeometry(0.3, 20, 20)
 sphereGeom.translate(0, 0.2, 0)
-
-// const screenGeom = wallGeom
-const defaultScreenMat = new THREE.MeshBasicMaterial({
-  color: new THREE.Color(0x555555),
-  side: THREE.DoubleSide,
-  transparent: true
-})
 
 function createVideoTexture(source, isSphere) {
   const videoTexture = new THREE.VideoTexture(source)
   videoTexture.minFilter = THREE.LinearFilter
   videoTexture.magFilter = THREE.LinearFilter
-  videoTexture.format = THREE.RGBFormat
 
   if (isSphere) {
     videoTexture.wrapS = THREE.RepeatWrapping
     videoTexture.wrapT = THREE.RepeatWrapping
-    videoTexture.repeat.set(6, 3)
+    videoTexture.repeat.set(4, 3)
   }
   return videoTexture
 }
@@ -43,14 +35,13 @@ function createVideoMaterial(info, isSphere) {
     console.log("TrackInfo: ", info)
     videoContainer = trackContainer
   } else {
-    const noiseVideoContainer = document.getElementById('video')
-    videoContainer = noiseVideoContainer
+    videoContainer = document.getElementById('video')
   }
 
   const videoTexture = createVideoTexture(videoContainer, isSphere)
   let screenMat = new THREE.MeshBasicMaterial({
     map: videoTexture,
-    side: THREE.DoubleSide
+    side: isSphere ? THREE.FrontSide : THREE.DoubleSide
   })
   return screenMat;
 }
@@ -62,7 +53,7 @@ export default function VideoScreen({x, y, dir, trackNo, type}) {
   const dist = isSphere ? 0 : 0.35
   x += dist * Direction.dx[dir];
   y += dist * Direction.dy[dir];
-  const rot = dir * Math.PI / 2
+  const rot = Math.PI * (isSphere ? 0.25 : 0.5 * dir)
 
   let geom = isSphere ? sphereGeom : screenGeom
   let screenMat = createVideoMaterial(info, isSphere);
