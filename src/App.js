213d5aacd890d2344445dffee598a63c568df857
Elmar Zander <elmar@zandere.de>
2022-12-18 17:27:25 +0100

    Moved styles and used bootstrap from node_modules in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import GameScreen from './views/GameScreen'
 import VideoController from './views/VideoController'
 import {useAsyncFinish} from './util/hooks'
 import {gameInit} from './game/GameLogic'
-import ButtonBar from "./dialogs/ButtonBar";
+import ButtonBar from "./dialogs/ButtonBar"
+
+import './assets/styles/inline-font-c64.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/bootstrap-custom.css'
 
 export default function App() {
   const finished = useAsyncFinish(gameInit)
